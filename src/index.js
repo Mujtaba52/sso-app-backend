b25f61b9277b532f8b66d66c02f07f58f936c1a0
Mujtaba Hassan <mujhassan786@gmail.com>
2024-05-27 03:43:46 +0500

    update index file

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import express from 'express';
 import "dotenv/config";
 import mongoose from 'mongoose';
 import orgRouter from './routes/organization.js';
+import userRouter from './routes/user.js';
+import authRouter from './routes/auth.js';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-
+app.use('/auth', authRouter);
 app.use('/organization', orgRouter);
+app.use('/user', userRouter);
 
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Database connected'))
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { ssoLogin } from '../controllers/auth.js';
+import { ssoLogin, ssoCallback } from '../controllers/auth.js';
 
 const authRouter = express.Router();
 
 
 authRouter.get("/sso-login", ssoLogin);
+authRouter.get("/sso-ssoCallback", ssoCallback);
 
 export default authRouter;
