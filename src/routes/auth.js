a8abe84f1ef5b91090ed8caee206a92b39a7c4ae
Mujtaba Hassan <mujhassan786@gmail.com>
2024-05-27 03:41:57 +0500

    add sso routes

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,2 +1,9 @@
 import express from 'express';
+import { ssoLogin } from '../controllers/auth.js';
+
 const authRouter = express.Router();
+
+
+authRouter.get("/sso-login", ssoLogin);
+
+export default authRouter;
