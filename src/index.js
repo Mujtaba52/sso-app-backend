a903c4c744fe433de5d4c8622405eaf45f273029
Mujtaba Hassan <mujhassan786@gmail.com>
2024-05-29 00:10:30 +0500

    enable cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import "dotenv/config";
 import mongoose from 'mongoose';
+import cors from "cors";
 import orgRouter from './routes/organization.js';
 import userRouter from './routes/user.js';
 import authRouter from './routes/auth.js';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -13,7 +15,7 @@ app.use('/auth', authRouter);
 app.use('/organization', orgRouter);
 app.use('/user', userRouter);
 
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true})
   .then(() => console.log('Database connected'))
   .catch((err) => console.log(err));
 
